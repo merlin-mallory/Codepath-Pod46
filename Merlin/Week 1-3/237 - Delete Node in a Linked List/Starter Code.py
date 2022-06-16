e38973257028e6420c67bc97c7f8e9af8950ec73
merlin-mallory <merlin.mallory.coder@gmail.com>
2022-06-16 18:56:46 +0900

    hasCycle attempt 1

--- Merlin/Week 1-3/237 - Delete Node in a Linked List/Starter Code.py
+++ Merlin/Week 1-3/237 - Delete Node in a Linked List/Starter Code.py
@@ -9,6 +9,16 @@ class Solution:
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
+
+        Input: head = [4,5,1,9], node = 5
+        Output: [4,1,9]
+
+        Input: head = [4,5,1,9], node = 1
+        Output: [4,5,9]
+
+        n ranges from 2 to 1000
+        Each value in the node is unique
+        node is guarenteed to be in list, and not be a tail node
         """
 
         node.val = node.next.val
