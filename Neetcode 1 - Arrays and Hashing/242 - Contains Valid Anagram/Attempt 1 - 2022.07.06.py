5fb639be2361e35f06c0f24b0cf740ee122981d0
merlin-mallory <merlin.mallory.coder@gmail.com>
2022-07-06 20:22:20 +0900

    001 - Two Sum

--- Neetcode 1 - Arrays and Hashing/242 - Contains Valid Anagram/Attempt 1 - 2022.07.06.py
+++ Neetcode 1 - Arrays and Hashing/242 - Contains Valid Anagram/Attempt 1 - 2022.07.06.py
@@ -17,4 +17,31 @@ class Solution:
         Constraints:
         1 <= s.length, t.length <= 5 * 10^4
         s and t consist of lowercase English letters.
+
+        Plan:
+        1. Create s_dict and t_dict (keys: letters, values: count)
+        2. Loop through both strings and create both dictionaries.
+        3. Loop through s_dict and check if there's a mismatch in t_dict. If a mismatch is found, then return False.
+        Otherwise return true.
         '''
+        from collections import defaultdict
+
+        s_dict = collections.defaultdict(int)
+        t_dict = collections.defaultdict(int)
+
+        for char in s:
+            s_dict[char] += 1
+
+        for char in t:
+            t_dict[char] += 1
+
+        for key in s_dict:
+            if s_dict[key] != t_dict[key]:
+                return False
+
+        for key in t_dict:
+            if t_dict[key] != s_dict[key]:
+                return False
+
+        return True
+
