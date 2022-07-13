505d9e64a90016b15769a9b379c4a4d698e61b4a
merlin-mallory <merlin.mallory.coder@gmail.com>
2022-07-13 15:38:43 +0900

    111 - Minimum Depth of Binary Tree

---
+++ Week 5 - Strings and Arrays/035 - Search Insert Position/Attempt 3 - 2022.07.13.py
@@ -0,0 +1,43 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        '''
+        https://leetcode.com/problems/search-insert-position/description/
+
+        Given a sorted array of distinct integers and a target value, return the index if the target is found.
+        If not, return the index where it would be if it were inserted in order.
+
+        You must write an algorithm with O(log n) runtime complexity.
+
+        Input: nums = [1,3,5,6], target = 5
+        Output: 2
+
+        Input: nums = [1,3,5,6], target = 2
+        Output: 1
+
+        Input: nums = [1,3,5,6], target = 7
+        Output: 4
+
+        Constraints:
+
+        1 <= nums.length <= 10^4
+        -10^4 <= nums[i] <= 10^4
+        nums contains distinct values sorted in ascending order.
+        -10^4 <= target <= 10^4
+
+        Plan:
+        1. Create two pointers and do binary search, being careful with the last iteration.
+        '''
+
+        left = 0
+        right = len(nums)-1
+
+        while left <= right:
+            mid = (left+right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid-1
+            else:  #nums[mid] < target
+                left = mid+1
+        # If the loop completes without finding the target, then the target is not in nums.
+        return left
