b3454ed3342594a67f55e86b7118dc5f34090fcd
merlin-mallory <merlin.mallory.coder@gmail.com>
2022-07-15 10:03:49 +0900

    347 - Top K Frequent Elements

--- Week 7 - Graphs p2/947 - Most Stones Removed with Same Row or Column/Attempt 1- 2022.07.14.py
+++ Week 7 - Graphs p2/947 - Most Stones Removed with Same Row or Column/Attempt 1- 2022.07.14.py
@@ -40,13 +40,35 @@ class Solution:
 
         Too hard, skipping for now.
         """
-        uf = {}
+        # DFS Answer
+        def dfs(i, j):
+            points.discard((i, j))
+            for y in rows[i]:
+                if (i, y) in points:
+                    dfs(i, y)
+            for x in cols[j]:
+                if (x, j) in points:
+                    dfs(x, j)
 
-        def find(x):
-            if x != uf.setdefault(x, x):
-                uf[x] = find(uf[x])
-            return uf[x]
-
+        points, island, rows, cols = {(i, j) for i, j in stones}, 0, collections.defaultdict(
+            list), collections.defaultdict(list)
         for i, j in stones:
-            uf[find(i)] = find(~j)
-        return len(stones) - len({find(x) for x in uf})
+            rows[i].append(j)
+            cols[j].append(i)
+        for i, j in stones:
+            if (i, j) in points:
+                dfs(i, j)
+                island += 1
+        return len(stones) - island
+
+        # Union find answer
+        # uf = {}
+        #
+        # def find(x):
+        #     if x != uf.setdefault(x, x):
+        #         uf[x] = find(uf[x])
+        #     return uf[x]
+        #
+        # for i, j in stones:
+        #     uf[find(i)] = find(~j)
+        # return len(stones) - len({find(x) for x in uf})
