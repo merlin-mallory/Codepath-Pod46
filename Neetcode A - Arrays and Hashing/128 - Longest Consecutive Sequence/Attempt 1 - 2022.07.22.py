220f02a79812325fbfb4e45ee25904adf0e76337
merlin-mallory <merlin.mallory.coder@gmail.com>
2022-07-22 19:25:31 +0900

    128 - Longest Consecutive Sequence

---
+++ Neetcode A - Arrays and Hashing/128 - Longest Consecutive Sequence/Attempt 1 - 2022.07.22.py
@@ -0,0 +1,35 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        """
+        https://leetcode.com/problems/longest-consecutive-sequence/
+
+        Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+        You must write an algorithm that runs in O(n) time.
+
+        Input: nums = [100,4,200,1,3,2]
+        Output: 4
+        Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+        Input: nums = [0,3,7,2,5,8,4,6,0,1]
+        Output: 9
+
+        Constraints:
+        0 <= nums.length <= 10^5
+        -10^9 <= nums[i] <= 10^9
+
+        1. Making a mapping_dict. Key: nums[i], Values: set of all values connected to the key
+        2. Loop through nums and create the mapping dict. If value+1 or value-1 is in the dictionary, then add loop[
+        i] to the appropriate key, and set the key = a copy of the appropriate set
+        """
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            # check if its the start of a sequence
+            if (n - 1) not in numSet:
+                length = 1
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
---
+++ Neetcode A - Arrays and Hashing/128 - Longest Consecutive Sequence/Starter Code.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        """
+        https://leetcode.com/problems/longest-consecutive-sequence/
+
+        Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+        You must write an algorithm that runs in O(n) time.
+
+        Input: nums = [100,4,200,1,3,2]
+        Output: 4
+        Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+        Input: nums = [0,3,7,2,5,8,4,6,0,1]
+        Output: 9
+
+        Constraints:
+        0 <= nums.length <= 10^5
+        -10^9 <= nums[i] <= 10^9
+
+        1. Making a mapping_dict. Key: nums[i], Values: set of all values connected to the key
+        2. Loop through nums and create the mapping dict. If value+1 or value-1 is in the dictionary, then add loop[
+        i] to the appropriate key, and set the key = a copy of the appropriate set
+        """
