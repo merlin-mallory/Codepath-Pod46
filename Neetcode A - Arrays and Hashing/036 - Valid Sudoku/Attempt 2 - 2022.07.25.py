2109cf53abdfa822fda00ac4fa1b7924e8d74b89
merlin-mallory <merlin.mallory.coder@gmail.com>
2022-07-25 21:42:34 +0900

    036 - Valid Sudoku

--- Neetcode A - Arrays and Hashing/036 - Valid Sudoku/Attempt 2 - 2022.07.25.py
+++ Neetcode A - Arrays and Hashing/036 - Valid Sudoku/Attempt 2 - 2022.07.25.py
@@ -16,7 +16,61 @@ class Solution:
         board.length == 9
         board[i].length == 9
         board[i][j] is a digit 1-9 or '.'.
+
+        1. Create empty my_set. Loop through each row in board[m][n]. If board[m][n] in my_set, then return False.
+        Otherwise continue. Add board[m][n] to my_set. After we complete each row, empty the set.
+        2. Loop through each column in board[m][n]. If board[m][n] in my_set, then return False. Otherwise continue.
+        Add board[m][n] to my_set. After we complete each row, empty the set.
+        3. Loop through each box in board[m][n]. Make a calculate_box function to take an upper left square,
+        and do the same set loop across the selected 3x3 grid. The 9 selected squares will be from a 3x3 loop:
+         (0,0), (0,3), (0,6), (3,0), (3,3), (3,6), (6,0), (6,3), (6,6).
         """
+        my_set = set()
+        rows = len(board)
+        columns = len(board[0])
+
+        # Check rows for duplicates
+        for m in range(rows):
+            for n in range(columns):
+                if board[m][n] in my_set:
+                    return False
+                if board[m][n] != ".":
+                    my_set.add(board[m][n])
+            my_set = set()
+
+        my_set = set()
+
+        # Check columns for duplicates
+        for n in range(columns):
+            for m in range(rows):
+                if board[m][n] in my_set:
+                    print("m, n, result", m, n, "False")
+                    return False
+                if board[m][n] != '.':
+                    my_set.add(board[m][n])
+            my_set = set()
+
+        def calculate_box(xcoord, ycoord):
+            my_set = set()
+            for m in range(3):
+                for n in range(3):
+                    if board[m+xcoord][n+ycoord] in my_set:
+                        return False
+                    if board[m+xcoord][n+ycoord] != ".":
+                        my_set.add(board[m+xcoord][n+ycoord])
+            return True
+
+        my_set = set()
+        my_result = None
+
+        # Check boxes for duplicates
+        for i in range(1, 10, 3):
+            for j in range(1, 10, 3):
+                my_result = calculate_box(i-1, j-1)
+                if my_result is False:
+                    return False
+        return True
+
 
 result = Solution()
 
