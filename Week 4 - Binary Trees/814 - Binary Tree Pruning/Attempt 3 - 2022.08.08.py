776278f5d08c468bc71f3104496a2d07a4ee6da2
merlin-mallory <merlin.mallory.coder@gmail.com>
2022-08-08 17:12:18 +0900

    814 - Binary Tree Pruning

--- Week 4 - Binary Trees/814 - Binary Tree Pruning/Attempt 3 - 2022.08.08.py
+++ Week 4 - Binary Trees/814 - Binary Tree Pruning/Attempt 3 - 2022.08.08.py
@@ -8,6 +8,7 @@ class Solution:
     def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         '''
         https://leetcode.com/problems/binary-tree-pruning/description/
+
         Given the root of a binary tree, return the same tree where every subtree (of the given tree) not containing a
         1 has been removed. A subtree of a node 'node' is 'node' plus every node that is a descendant of node.
 
@@ -27,20 +28,17 @@ class Solution:
         The number of nodes in the tree is in the range [1, 200].
         Node.val is either 0 or 1.
 
-        Plan:
-        1. DFS post-order traverse the tree, deleteing nodes that contain zero from the bottom of the tree towards
-        the top.
-        2. Return the resultant tree
+        1. DFS post-order search. If the node is not equal to 1 and doesn't have any children, then remove it.
+        2. Time: O(n), Space: O(1)
         '''
-        current_node = root
 
-        def helper(current_node):
-            if current_node.left:
-                current_node.left = helper(current_node.left)
-            if current_node.right:
-                current_node.right = helper(current_node.right)
-            if current_node.val != 1 and current_node.left is None and current_node.right is None:
-                current_node = None
-            return current_node
+        if root.left:
+            root.left = self.pruneTree(root.left)
+        if root.right:
+            root.right = self.pruneTree(root.right)
+        if root.val != 1 and root.left is None and root.right is None:
+            return None
 
-        return helper(root)
+        return root
+
+
--- Week 4 - Binary Trees/814 - Binary Tree Pruning/Starter Code.py
+++ Week 4 - Binary Trees/814 - Binary Tree Pruning/Starter Code.py
@@ -8,6 +8,7 @@ class Solution:
     def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         '''
         https://leetcode.com/problems/binary-tree-pruning/description/
+
         Given the root of a binary tree, return the same tree where every subtree (of the given tree) not containing a
         1 has been removed. A subtree of a node 'node' is 'node' plus every node that is a descendant of node.
 
@@ -26,21 +27,5 @@ class Solution:
         Constraints:
         The number of nodes in the tree is in the range [1, 200].
         Node.val is either 0 or 1.
-
-        Plan:
-        1. DFS post-order traverse the tree, deleteing nodes that contain zero from the bottom of the tree towards
-        the top.
-        2. Return the resultant tree
         '''
-        current_node = root
 
-        def helper(current_node):
-            if current_node.left:
-                current_node.left = helper(current_node.left)
-            if current_node.right:
-                current_node.right = helper(current_node.right)
-            if current_node.val != 1 and current_node.left is None and current_node.right is None:
-                current_node = None
-            return current_node
-
-        return helper(root)
