9d5e99b8e6f2ac1a1b9ae70c92d083679dfb0171
merlin-mallory <merlin.mallory.coder@gmail.com>
2022-08-11 22:32:56 +0900

    1099 - Two Sum Less Than K

---
+++ CapitalOne/1099 - Two Sum Less Than K/Attempt 3 - 2022.08.11.py
@@ -0,0 +1,39 @@
+class Solution:
+    from typing import List
+    def twoSumLessThanK(self, nums: List[int], k: int) -> int:
+        """
+        Given an array nums of integers and integer k, return the maximum sum such that there exists i < j with nums[
+        i] + nums[j] = sum and sum < k. If no i, j exist satisfying this equation, return -1.
+
+        Input: nums = [34,23,1,24,75,33,54,8], k = 60
+        Output: 58
+        Explanation: We can use 34 and 24 to sum 58 which is less than 60.
+
+        Input: nums = [10,20,30], k = 15
+        Output: -1
+        Explanation: In this case it is not possible to get a pair sum less that 15.
+
+        Constraints:
+        1 <= nums.length <= 100
+        1 <= nums[i] <= 1000
+        1 <= k <= 2000
+
+        Plan 1:
+        1. Outer loop through nums, inner loop through nums. If i != j and nums[i]+nums[j] < k, then max_sum = max(
+        max_sum, result).
+        2. Time: O(n^2), Space: O(1)
+        """
+        max_sum = -1
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                current_sum = nums[i] + nums[j]
+                if i != j and current_sum < k:
+                    max_sum = max(max_sum, current_sum)
+
+        return max_sum
+
+
+
+result = Solution()
+print(result.twoSumLessThanK([34,23,1,24,75,33,54,8], 60))  # 58
+print(result.twoSumLessThanK([10,20,30], 15))               # -1
