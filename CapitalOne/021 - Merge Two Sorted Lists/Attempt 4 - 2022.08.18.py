02c484e301e765aa97d9f1394f45d4b16109ddcf
merlin-mallory <merlin.mallory.coder@gmail.com>
2022-08-18 02:18:50 +0900

    021 - Merge Two Sorted Lists

---
+++ CapitalOne/021 - Merge Two Sorted Lists/Attempt 4 - 2022.08.18.py
@@ -0,0 +1,62 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        You are given the heads of two sorted linked lists list1 and list2.
+
+        Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first
+        two lists.
+
+        Return the head of the merged linked list.
+
+        Input: list1 = [1,2,4], list2 = [1,3,4]
+        Output: [1,1,2,3,4,4]
+        Example 2:
+
+        Input: list1 = [], list2 = []
+        Output: []
+        Example 3:
+
+        Input: list1 = [], list2 = [0]
+        Output: [0]
+
+        Constraints:
+        The number of nodes in both lists is in the range [0, 50].
+        -100 <= Node.val <= 100
+        Both list1 and list2 are sorted in non-decreasing order.
+
+        Plan:
+        1. Create list3, dummy = list3. Loop through list1 and list2, at each step adding the lesser value node to
+        list3.
+        2. Return the dummy.
+        """
+        if not list1:
+            return list2
+
+        if not list2:
+            return list1
+
+        list3 = ListNode(-1)
+        dummy = list3
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                list3.next = list1
+                list1 = list1.next
+                list3 = list3.next
+            else:
+                list3.next = list2
+                list2 = list2.next
+                list3 = list3.next
+
+        if list1:
+            list3.next = list1
+
+        if list2:
+            list3.next = list2
+
+        return dummy.next
