79e8b41530bf26eac20ada64d72ca4b0ce9df53d
merlin-mallory <merlin.mallory.coder@gmail.com>
2022-08-20 01:52:24 +0900

    015 - 3Sum

---
+++ Neetcode B - Two Pointers/3Sum/Attempt 1 - 2022.08.20.py
@@ -0,0 +1,80 @@
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        """
+        https://leetcode.com/problems/3sum/
+
+        Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k,
+        and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+        Notice that the solution set must not contain duplicate triplets.
+
+        Input: nums = [-1,0,1,2,-1,-4]
+        Output: [[-1,-1,2],[-1,0,1]]
+        Explanation:
+        nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+        nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+        nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+        The distinct triplets are [-1,0,1] and [-1,-1,2].
+        Notice that the order of the output and the order of the triplets does not matter.
+
+        Input: nums = [0,1,1]
+        Output: []
+        Explanation: The only possible triplet does not sum up to 0.
+
+        Input: nums = [0,0,0]
+        Output: [[0,0,0]]
+        Explanation: The only possible triplet sums up to 0.
+
+        Constraints:
+        3 <= nums.length <= 3000
+        -10^5 <= nums[i] <= 10^5
+
+        Plan:
+        1. Brute force it. Three loops, use conditionals to optimize a bit. Whenever a result is found, convert it to a
+        set, and check if that collection of numbers is in added_set. If it's not in added_set, then append it to the
+        final array.
+        """
+        res = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        return res
+
+        # Failed attempt
+        # final_arr = []
+        # added_set = set()
+        #
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)):
+        #         if i == j:
+        #             continue
+        #         for k in range(len(nums)):
+        #             if i == k or j == k:
+        #                 continue
+        #             else:
+        #                 current_sum = nums[i] + nums[j] + nums[k]
+        #                 current_arr = [nums[i], nums[j], nums[k]]
+        #                 current_arr.sort()
+        #                 current_tup = tuple(current_arr)
+        #                 if current_sum == 0 and current_tup not in added_set:
+        #                     final_arr.append(current_arr)
+        #                     added_set.add(current_tup)
+        # return final_arr
