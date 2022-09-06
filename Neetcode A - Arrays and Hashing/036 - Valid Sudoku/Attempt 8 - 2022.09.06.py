e48c4c7ee3456e5fca2d4e984106fb833cfbf0fd
merlin-mallory <merlin.mallory.coder@gmail.com>
2022-09-06 20:18:54 +0900

    036 - Valid Sudoku

---
+++ Neetcode A - Arrays and Hashing/036 - Valid Sudoku/Attempt 8 - 2022.09.06.py
@@ -0,0 +1,88 @@
+class Solution:
+    def isValidSudoku(self, board) -> bool:
+        """
+        Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the
+        following rules:
+
+        Each row must contain the digits 1-9 without repetition.
+        Each column must contain the digits 1-9 without repetition.
+        Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+        Note:
+
+        A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+        Only the filled cells need to be validated according to the mentioned rules.
+
+        Constraints:
+        board.length == 9
+        board[i].length == 9
+        board[i][j] is a digit 1-9 or '.'.
+        """
+        rows_set = [set() for _ in range(9)]
+        columns_set = [set() for _ in range(9)]
+        box_set = [set() for _ in range(9)]
+
+        for m in range(9):
+            for n in range(9):
+                current_square = board[m][n]
+                if current_square == ".":
+                    continue
+
+                if current_square in rows_set[m]:
+                    return False
+                else:
+                    rows_set[m].add(current_square)
+
+                if current_square in columns_set[n]:
+                    return False
+                else:
+                    columns_set[n].add(current_square)
+
+                box_calc = (m // 3) * 3 + (n // 3)
+
+                if current_square in box_set[box_calc]:
+                    return False
+                else:
+                    box_set[box_calc].add(current_square)
+
+        return True
+
+
+result = Solution()
+
+board1 = [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+    , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+    , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+    , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+    , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+    , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+    , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+    , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+    , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+
+print(result.isValidSudoku(board1))  # True
+
+board2 = [["8", "3", ".", ".", "7", ".", ".", ".", "."],
+          ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+          [".", "9", "8", ".", ".", ".", ".", "6", "."],
+          ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+          ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+          ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+          [".", "6", ".", ".", ".", ".", "2", "8", "."],
+          [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+          [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+
+print(result.isValidSudoku(board2))  # False
+# Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8.
+# Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+board3 = [[".", ".", ".", ".", "5", ".", ".", "1", "."],
+          [".", "4", ".", "3", ".", ".", ".", ".", "."],
+          [".", ".", ".", ".", ".", "3", ".", ".", "1"],
+          ["8", ".", ".", ".", ".", ".", ".", "2", "."],
+          [".", ".", "2", ".", "7", ".", ".", ".", "."],
+          [".", "1", "5", ".", ".", ".", ".", ".", "."],
+          [".", ".", ".", ".", ".", "2", ".", ".", "."],
+          [".", "2", ".", "9", ".", ".", ".", ".", "."],
+          [".", ".", "4", ".", ".", ".", ".", ".", "."]]
+
+print(result.isValidSudoku(board3))  # False
