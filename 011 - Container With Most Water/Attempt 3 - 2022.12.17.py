db64caa2dd354c7ae4d38e798e6998b833702d6a
merlin-mallory <merlin.mallory.coder@gmail.com>
2022-12-17 18:53:06 +0900

    011 - Container With Most Water

--- 011 - Container With Most Water/Attempt 3 - 2022.12.17.py
+++ 011 - Container With Most Water/Attempt 3 - 2022.12.17.py
@@ -25,7 +25,24 @@ class Solution:
         n == height.length
         2 <= n <= 10^5
         0 <= height[i] <= 10^4
+
+        Plan:
+        1. Looks like Two Pointers. l = 0, r = len(height), max_area = 0.
+        2. Loop while l < r:
+            3. Calculate the current area: (r-l) * min(height[l], height[r])
+            4. Update max_area.
+            5. Move the pointer with the relatively shorter height.
+        6. Return max_area
         """
+        l, r, max_area = 0, len(height)-1, 0
+        while l < r:
+            current_area = (r-l) * min(height[l], height[r])
+            max_area = max(max_area, current_area)
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return max_area
 
 result = Solution()
 print(result.maxArea([1,8,6,2,5,4,8,3,7]))  # 49
