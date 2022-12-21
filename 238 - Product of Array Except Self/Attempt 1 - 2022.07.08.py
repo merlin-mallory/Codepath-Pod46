f83cbd89d5659f4466c35c16670142596cd17314
merlin-mallory <merlin.mallory.coder@gmail.com>
2022-12-22 00:18:55 +0900

    128 - Longest Consecutive Sequence

--- 238 - Product of Array Except Self/Attempt 1 - 2022.07.08.py
+++ 238 - Product of Array Except Self/Attempt 1 - 2022.07.08.py
@@ -74,3 +74,15 @@ class Solution:
         #     ret[index_of_zero] = int(zeroed_product)
         #
         # return ret
+
+    # Neetcode
+    #         res = [1] * (len(nums))
+    #         prefix = 1
+    #         for i in range(len(nums)):
+    #             res[i] = prefix
+    #             prefix *= nums[i]
+    #         postfix = 1
+    #         for i in range(len(nums) - 1, -1, -1):
+    #             res[i] *= postfix
+    #             postfix *= nums[i]
+    #         return res
