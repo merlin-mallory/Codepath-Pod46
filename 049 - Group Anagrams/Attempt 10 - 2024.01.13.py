c443f1baf32058e8d90d3facdf3a756281a6595e
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-01-13 15:34:25 +0900

    49 - Group Anagrams

---
+++ 049 - Group Anagrams/Attempt 10 - 2024.01.13.py
@@ -0,0 +1,31 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+        https://leetcode.com/problems/group-anagrams/
+
+        Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+        An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically
+        using all the original letters exactly once.
+
+        Input: strs = ["eat","tea","tan","ate","nat","bat"]
+        Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+        Input: strs = [""]
+        Output: [[""]]
+
+        Input: strs = ["a"]
+        Output: [["a"]]
+
+        Constraints:
+
+        1 <= strs.length <= 10^4
+        0 <= strs[i].length <= 100
+        strs[i] consists of lowercase English letters.
+        '''
+        import collections
+        anagram_dict = collections.defaultdict(list)
+
+        for str in strs:
+            anagram_dict[tuple(sorted(str))].append(str)
+        return anagram_dict.values()
