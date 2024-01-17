66b5ae5c836a7bc394e5b01553910df5dbc5baef
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-01-17 11:12:55 +0900

    121- Best Time to Buy And Sell Stock

---
+++ 042 - Trapping Rain Water/Attempt 3 - 2024.01.15.py
@@ -0,0 +1,50 @@
+from typing import List
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        """
+        https://leetcode.com/problems/trapping-rain-water/
+
+        Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much
+        water it can trap after raining.
+
+        Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+        Output: 6
+
+        Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
+        In this case, 6 units of rain water (blue section) are being trapped.
+
+        Input: height = [4,2,0,3,2,5]
+        Output: 9
+
+        Constraints:
+        n == height.length
+        1 <= n <= 2 * 10^4
+        0 <= height[i] <= 10^5
+
+        Plan:
+        Two pointers solution
+        1. Initialize pointers. l = 0, r=0, volume = 0
+        """
+        l, r, total_water = 0, len(height) - 1, 0
+        left_max, right_max = height[l], height[r]
+
+        while l < r:
+            if height[l] <= height[r]:
+                l += 1
+                if left_max > height[l]:
+                    total_water += left_max - height[l]
+                else:
+                    left_max = height[l]
+            else:
+                r -= 1
+                if right_max > height[r]:
+                    total_water += right_max - height[r]
+                else:
+                    right_max = height[r]
+
+        return total_water
+
+result = Solution()
+print(result.trap([0,1,0,2,1,0,1,3,2,1,2,1]))   # 6
+print(result.trap([4,2,0,3,2,5]))               # 9
---
+++ 121 - Best Time to Buy and Sell Stock/Attempt 8 - 2024.01.17.py
@@ -0,0 +1,47 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+        You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the
+        future to sell that stock.
+
+        Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+        Input: prices = [7,1,5,3,6,4]
+        Output: 5
+        Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+        Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+        Input: prices = [7,6,4,3,1]
+        Output: 0
+        Explanation: In this case, no transactions are done and the max profit = 0.
+
+        Constraints:
+
+        1 <= prices.length <= 10^5
+        0 <= prices[i] <= 10^4
+
+        Plan:
+        Sliding Window
+        Brute Force
+        1. Loop through each value of prices as buy date, second loop to i+1 with sell date, keep track of the max
+        profit.
+        """
+        min_price = float('inf')
+        max_profit = 0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            elif prices[i] - min_price > max_profit:
+                max_profit = prices[i] - min_price
+        return maxP
+
+
+result = Solution()
+print(result.maxProfit([7,1,5,3,6,4]))  # 5
+print(result.maxProfit([7,6,4,3,1]))    # 0
+print(result.maxProfit([1,4,2]))        # 3
+print(result.maxProfit([0,3,8,6,8,6,6,8,2,0,2,7]))  # 8
