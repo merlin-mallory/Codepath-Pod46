c081c039c74b8a347e547f45e7a9233b3909819d
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-01-18 20:22:18 +0900

    424 - Longest Repeating Character Replacement

---
+++ 424 - Longest Repeating Character Replacement/Attempt 7 - 2024.01.17.py
@@ -0,0 +1,66 @@
+import collections
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        '''
+        https://leetcode.com/problems/longest-repeating-character-replacement/
+
+        You are given a string s and an integer k.
+        You can choose any character of the string and change it to any other uppercase English character.
+        You can perform this operation at most k times.
+
+        Return the length of the longest substring containing the same letter you can get after performing the above
+        operations.
+
+        Input: s = "ABAB", k = 2
+        Output: 4
+        Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+        Input: s = "AABABBA", k = 1
+        Output: 4
+        Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+        The substring "BBBB" has the longest repeating letters, which is 4.
+
+        Constraints:
+
+        1 <= s.length <= 105
+        s consists of only uppercase English letters.
+        0 <= k <= s.length
+
+        Plan:
+        Sliding Window
+        1. Create a dict (keys: chars, values: count of that char in the current substring)
+        2. Initialize left pointer, current_count
+        3. Right pointer loops until the end of the array
+            4. Check if s[r] is in dict.
+                5. If dict[r] < k
+                    6. dict[r] += 1
+                    7. current_count += 1
+                    8. Update max
+                8. Else
+                    9. dict[l] -= 1
+                    10. current_count -= 1
+            11. Else
+                12. dict[r] = 1
+                13. current_count += 1
+                13. Update max
+        14. Return
+        '''
+        count = {}
+        res = 0
+
+        l = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+
+            while ((r-l-1) - max(count.values())) > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res
+
+result = Solution()
+print(result.characterReplacement("ABAB", 2))       # 4
+print(result.characterReplacement("AABABBA", 1))    # 4
+
