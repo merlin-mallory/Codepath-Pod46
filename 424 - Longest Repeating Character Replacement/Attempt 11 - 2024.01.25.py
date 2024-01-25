4ecfa4f98d023dda558672f297062df6c043e9d0
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-01-25 16:29:41 +0900

    567 - Permutation in String

--- 424 - Longest Repeating Character Replacement/Attempt 11 - 2024.01.25.py
+++ 424 - Longest Repeating Character Replacement/Attempt 11 - 2024.01.25.py
@@ -27,9 +27,35 @@ class Solution:
 
         Plan:
         Sliding Window
-        1. Create window_dict. Keys: Chars in window, Values: Count of keys.
-        2. Loop while l 
+        1. l = 0, longest_substring = [-1, -1]
+        2. Handle s == "".
+        3. Create window_dict. Keys: Chars in window, Values: Count of keys.
+        4. Loop r to end of s.
+            5. Add s[r] to window_dict.
+            6. Check if max(window_dict) <= k (this means its a valid window)
+                7. Calculate the current_len
+                8. Update longest_substring.
+            9. Otherwise, remove window_dict[l] and l+ 1 until that conditional is true.
+        10. Deconstruct longest_substring and return max_len
         '''
+
+        import collections
+        counts = collections.Counter()
+        max_len = 0
+        l = 0
+
+        for r in range(len(s)):
+            counts[s[r]] += 1
+            current_len = r - l + 1
+
+            if (current_len - max(counts.values())) > k:
+                counts[s[l]] -= 1
+                l += 1
+            else:
+                max_len = max(max_len, current_len)
+
+        return max_len
+
 
 result = Solution()
 print(result.characterReplacement("ABAB", 2))       # 4
