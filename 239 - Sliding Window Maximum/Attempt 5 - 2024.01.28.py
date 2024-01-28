c1f69356c80a1015f55104128c2044629a0394ed
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-01-28 16:48:07 +0900

    239 - Sliding Window Maximum

---
+++ 239 - Sliding Window Maximum/Attempt 5 - 2024.01.28.py
@@ -0,0 +1,77 @@
+from typing import List
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        '''
+        239. Sliding Window Maximum
+
+        https://leetcode.com/problems/sliding-window-maximum/
+
+        You are given an array of integers nums, there is a sliding window of size k which is moving from the very
+        left of the array to the very right. You can only see the k numbers in the window. Each time the sliding
+        window moves right by one position.
+
+        Return the max sliding window.
+
+        Example 1:
+
+        Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+        Output: [3,3,5,5,6,7]
+        Explanation:
+        Window position                Max
+        ---------------               -----
+        [1  3  -1] -3  5  3  6  7       3
+         1 [3  -1  -3] 5  3  6  7       3
+         1  3 [-1  -3  5] 3  6  7       5
+         1  3  -1 [-3  5  3] 6  7       5
+         1  3  -1  -3 [5  3  6] 7       6
+         1  3  -1  -3  5 [3  6  7]      7
+        Example 2:
+
+        Input: nums = [1], k = 1
+        Output: [1]
+
+        Constraints:
+
+        1 <= nums.length <= 10^5
+        -10^4 <= nums[i] <= 10^4
+        1 <= k <= nums.length
+
+        Plan:
+        Sliding Window
+        1. Create deque which will contain the maximum value in the zero index, and remainder of the
+        values will be indexes of nums where the values are stored in decreasing order.
+        2. Initialize final_arr = [], l = 0, r = 0
+        3. Loop r through nums
+            4. If nums[r] > nums[deque[-1]], then we can pop and discard deque[-1]
+            5. If deque[0] < l, then we can popleft and discard deque[0]
+            6. Append (s[r]) to the deque.
+            7. If r >= k, then append deque[0] to the final_arr, and l++
+        8. Return final_arr
+        '''
+        final_arr = []
+        import collections
+        deque = collections.deque()
+        l, r = 0, 0
+
+        while r < len(nums):
+            while deque and l > deque[0]:
+                deque.popleft()
+
+            while deque and nums[r] > nums[deque[-1]]:
+                deque.pop()
+
+            deque.append(r)
+
+            if r+1 >= k:
+                final_arr.append(nums[deque[0]])
+                l += 1
+
+            r += 1
+
+        return final_arr
+
+
+result = Solution()
+
+print(result.maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))  # [3,3,5,5,6,7]
+print(result.maxSlidingWindow([1], 1))                  # [1]
