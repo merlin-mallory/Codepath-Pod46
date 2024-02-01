d435f3e912aff8d6614ba54d6797d41382fd9f69
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-02-01 19:02:20 +0900

    567 Permutation in String

---
+++ 567 - Permutation in String/Attmpt 8 - 2024.02.01.py
@@ -0,0 +1,60 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        """
+        https://leetcode.com/problems/permutation-in-string/
+
+        Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+        In other words, return true if one of s1's permutations is the substring of s2.
+
+        Input: s1 = "ab", s2 = "eidbaooo"
+        Output: true
+        Explanation: s2 contains one permutation of s1 ("ba").
+
+        Input: s1 = "ab", s2 = "eidboaoo"
+        Output: false
+
+        Constraints:
+        1 <= s1.length, s2.length <= 104
+        s1 and s2 consist of lowercase English letters.
+
+        Plan:
+        1. Create s1_counts. Init with s1. Keys: Chars in s1, Values: Count of keys.
+        2. Create s2_window. Init empty. Keys: Chars in s2's current window, Values: Count of keys.
+        3. l = 0, r = 0
+        4. have = 0, need = len(s1_counts
+        4. while r < len(s2)
+            5. Add s2[r] to s2_window.
+            6. If s2_window[s2[r]] == s1_counts[s2[r]:
+                7. have++
+            8. if r > len(s1):
+                9. s2_window[s2[l] -= 1, update have, l++
+            10 if s2_window == s1_counts, return True
+        11. Return False
+        Edge Cases: None
+        Time: O(n)
+        Space: O(n)
+        """
+        import collections
+        s1_counts = collections.Counter(s1)
+        s2_window = collections.Counter()
+        l, r = 0, 0
+        while r < len(s2):
+            s2_window[s2[r]] += 1
+
+            if r >= len(s1):
+                if s2_window[s2[l]] == 1:
+                    del(s2_window[s2[l]])
+                else:
+                    s2_window[s2[l]] -= 1
+                l += 1
+
+            if s1_counts == s2_window:
+                return True
+            r += 1
+        return False
+
+
+result = Solution()
+print(result.checkInclusion("ab", "eidbaooo"))  # True
+print(result.checkInclusion("ab", "eidboaoo"))  # False
