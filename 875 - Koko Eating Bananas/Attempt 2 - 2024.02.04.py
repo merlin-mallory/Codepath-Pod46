c20603ee03a9f1d8390f26ce0ab3fdd9ebd7819d
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-02-04 17:24:53 +0900

    875 Koko Eating Bananas

--- 875 - Koko Eating Bananas/Attempt 2 - 2024.02.04.py
+++ 875 - Koko Eating Bananas/Attempt 2 - 2024.02.04.py
@@ -29,11 +29,37 @@ class Solution:
         1 <= piles.length <= 10^4
         piles.length <= h <= 10^9
         1 <= piles[i] <= 10^9
+
+        Plan:
+        Binary Search
+        1. Loop through piles and set max_complete_k = max(piles). Init min_k = max(piles).
+        2. Do a binary search between 1 and max_complete_k. At each iteration, see if Koko can complete eating all
+        the bananas in time successfuly. If success, then set l = m + 1. If fail, set r = m - 1. Update min_k.
+        3. Return min_k.
         '''
+        import math
+        min_k = 0
+        for i in range(len(piles)):
+            if piles[i] > min_k:
+                min_k = piles[i]
+
+        l, r = 1, min_k
+        while l < r:
+            m = (l+r)//2
+            time_to_finish = 0
+            for i in range(len(piles)):
+                time_to_finish += math.ceil(piles[i] / m)
+
+            if time_to_finish <= h:
+                r = m
+            else:
+                l = m + 1
+
+        return r
+
 
 result = Solution()
 print(result.minEatingSpeed([3,6,7,11], 8))         # 4
 print(result.minEatingSpeed([30,11,23,4,20], 5))    # 30
 print(result.minEatingSpeed([30,11,23,4,20], 6))    # 23
 print(result.minEatingSpeed([312884470],312884469)) # 2
-
