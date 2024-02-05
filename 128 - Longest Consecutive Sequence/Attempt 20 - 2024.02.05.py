d6f59833b9561457882d0270644f447dd6155c53
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-02-05 11:41:48 +0900

    128 Longest Consecutive Sequence

---
+++ 128 - Longest Consecutive Sequence/Attempt 20 - 2024.02.05.py
@@ -0,0 +1,41 @@
+from typing import List
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        """
+        https://leetcode.com/problems/longest-consecutive-sequence/
+
+        Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+        You must write an algorithm that runs in O(n) time.
+
+        Input: nums = [100,4,200,1,3,2]
+        Output: 4
+        Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+        Input: nums = [0,3,7,2,5,8,4,6,0,1]
+        Output: 9
+
+        Constraints:
+        0 <= nums.length <= 10^5
+        -10^9 <= nums[i] <= 10^9
+
+        Plan:
+        1. Create nums_set.
+        2. Init longest_consecutive_seq = 0.
+        3. Loop through nums.
+            If nums[i] is not in nums_set, then we've located the start of a sequence. So set a counter,
+            and loop upwards until the end of the sequence, and update longest_consecutive_seq. At each step,
+            remove nums[i+counter] in order to improve time complexity.
+            Otherwise, just continue.
+        """
+
+        nums_set = set(nums)
+        longest_consecutive_seq = 0
+        for num in nums:
+            if num-1 not in nums_set:
+                counter = 1
+                while (num+counter) in nums_set:
+                    nums_set.remove(num+counter)
+                    counter += 1
+                longest_consecutive_seq = max(longest_consecutive_seq, counter)
+        return longest_consecutive_seq
