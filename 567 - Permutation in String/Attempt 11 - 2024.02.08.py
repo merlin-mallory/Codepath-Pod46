6c152458a4993809e0167a0e391a7d513bea9266
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-02-08 15:50:00 +0900

    Permutation in String

---
+++ 567 - Permutation in String/Attempt 11 - 2024.02.08.py
@@ -0,0 +1,52 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        """
+        https://leetcode.com/problems/permutation-in-string/
+
+        Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+        In other words, return true if one of s1's permutations is the substring of s2.
+
+        Input: s1 = "ab", s2 = "eidbaooo"
+        Output: true
+        Explanation: s2 contains one permutation of s1 ("ba").
+
+        Input: s1 = "ab", s2 = "eidboaoo"
+        Output: false
+
+        Constraints:
+        1 <= s1.length, s2.length <= 104
+        s1 and s2 consist of lowercase English letters.
+
+        Plan:
+        Sliding Window
+        s1_counts dict.
+        s2_window dict.
+        Loop through s2.
+            Update s2_window's r and l pointers
+            If s1_counts = s2_window, return true
+        Otherwise return False
+        Time: O(n)
+        Space: O(1)
+        Edge: None
+        """
+        import collections
+        s1_counts = collections.Counter(s1)
+        s2_window = collections.Counter()
+        l, r = 0, 0
+        while r < len(s2):
+            s2_window[s2[r]] += 1
+            if r+1 > len(s1):
+                if s2_window[s2[l]] == 1:
+                    del(s2_window[s2[l]])
+                else:
+                    s2_window[s2[l]] -= 1
+                l += 1
+            if s2_window == s1_counts:
+                return True
+            r += 1
+        return False
+
+result = Solution()
+print(result.checkInclusion("ab", "eidbaooo"))  # True
+print(result.checkInclusion("ab", "eidboaoo"))  # False
