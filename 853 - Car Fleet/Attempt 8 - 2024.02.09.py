6d74474fe1ffd151cc68b2aeb97c4c256e4d5e21
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-02-09 14:02:17 +0900

    853 Car Fleet

---
+++ 853 - Car Fleet/Attempt 8 - 2024.02.09.py
@@ -0,0 +1,81 @@
+from typing import List
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        '''
+        There are n cars going to the same destination along a one-lane road. The destination is target miles away.
+
+        You are given two integer array position and speed, both of length n, where position[i] is the position of the
+        ith car and speed[i] is the speed of the ith car (in miles per hour).
+
+        A car can never pass another car ahead of it, but it can catch up to it and drive bumper to bumper at the
+        same speed. The faster car will slow down to match the slower car's speed. The distance between these two cars
+        is ignored (i.e., they are assumed to have the same position).
+
+        A car fleet is some non-empty set of cars driving at the same position and same speed. Note that a single car
+        is also a car fleet.
+
+        If a car catches up to a car fleet right at the destination point, it will still be considered as one car fleet.
+
+        Return the number of car fleets that will arrive at the destination.
+
+        Example 1:
+        Input: target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
+        Output: 3
+        Explanation:
+        The cars starting at 10 (speed 2) and 8 (speed 4) become a fleet, meeting each other at 12.
+        The car starting at 0 does not catch up to any other car, so it is a fleet by itself.
+        The cars starting at 5 (speed 1) and 3 (speed 3) become a fleet, meeting each other at 6. The fleet moves at
+        speed 1 until it reaches target.
+        Note that no other cars meet these fleets before the destination, so the answer is 3.
+
+        Example 2:
+        Input: target = 10, position = [3], speed = [3]
+        Output: 1
+        Explanation: There is only one car, hence there is only one fleet.
+
+        Example 3:
+        Input: target = 100, position = [0,2,4], speed = [4,2,1]
+        Output: 1
+        Explanation:
+        The cars starting at 0 (speed 4) and 2 (speed 2) become a fleet, meeting each other at 4. The fleet moves at
+        speed 2.
+        Then, the fleet (speed 2) and the car starting at 4 (speed 1) become one fleet, meeting each other at 6. The
+        fleet moves at speed 1 until it reaches target.
+
+        Constraints:
+        n == position.length == speed.length
+        1 <= n <= 10^5
+        0 < target <= 10^6
+        0 <= position[i] < target
+        All the values of position are unique.
+        0 < speed[i] <= 10^6
+
+        Plan:
+        Stack
+        1. Create cars array. Contains [position[i], time_to_finish] for each index in cars and speed.
+        2. Reverse sort the cars array, so we can process the car closest to the finish first, followed by the rest
+        in sequential order.
+        3. Loop through the cars array. Append the cur_car to the stack. Then compare, is stack[-1][1] < stack[-2][
+        1]? If so, that means the most recently appended car to the stack will form a fleet with the stack[-2] car.
+        So pop the top of the stack to remove it from consideration.
+        4. After the loop finishes, the length of the array will provide the number of fleets, so return it.
+        Time: O(n)
+        Space: O(n)
+        Edge: None
+        '''
+        stack = []
+        cars = []
+        for i in range(len(position)):
+            cars.append([position[i], (target - position[i])/speed[i]])
+        cars = sorted(cars)[::-1]
+        for car in cars:
+            stack.append(car)
+            if len(stack) > 1 and (stack[-1][1] <= stack[-2][1]):
+                stack.pop()
+        return len(stack)
+
+
+result = Solution()
+print(result.carFleet(12, [10,8,0,5,3], [2,4,1,1,3]))   # 3
+print(result.carFleet(10, [3], [3]))                    # 1
+print(result.carFleet(100, [0,2,4], [4,2,1]))           # 1
