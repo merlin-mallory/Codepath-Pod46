7ea4439dd536c494413beb81156cc7bae757dc9c
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-02-15 22:02:44 +0900

    424 Longest Repeating Character

--- 424 - Longest Repeating Character Replacement/Attempt 17 - 2024.02.15.py
+++ 424 - Longest Repeating Character Replacement/Attempt 17 - 2024.02.15.py
@@ -1,8 +1,6 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         '''
-        424 - Longest Repeating Character Replacement
-
         https://leetcode.com/problems/longest-repeating-character-replacement/
 
         You are given a string s and an integer k.
@@ -26,7 +24,35 @@ class Solution:
         1 <= s.length <= 105
         s consists of only uppercase English letters.
         0 <= k <= s.length
+
+        Plan:
+        Sliding Window
+        Create s_window. Init empty.
+        l, r = 0, 0
+        Loop while r < len(s).
+            Add s[r] to s_window
+            Loop while (r - l + 1 - max(s_window.values()) <= k.
+                Calc cur_len, update max_len, remove s[l] from s_window, l++.
+            r++
+        return max_len
+
+        Time: O(n)
+        Space: O(1)
+        Edge: None
         '''
+        import collections
+        s_window = collections.Counter()
+        max_len = 0
+        l, r = 0, 0
+        while r < len(s):
+            s_window[s[r]] += 1
+            if (r-l+1 - max(s_window.values())) <= k:
+                max_len = max(max_len, r-l+1)
+            else:
+                s_window[s[l]] -= 1
+                l += 1
+            r += 1
+        return max_len
 
 result = Solution()
 print(result.characterReplacement("ABAB", 2))       # 4
