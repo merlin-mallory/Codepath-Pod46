4cd64541cc1a99d8fb934622fd357b748de67328
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-02-16 13:43:45 +0900

    146 LRU Cache

---
+++ 146 - LRU Cache/Attempt 5 - 2024.02.16.py
@@ -0,0 +1,72 @@
+class LRUCache:
+    '''
+    Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+    Implement the LRUCache class:
+
+    LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+    int get(int key) Return the value of the key if the key exists, otherwise return -1.
+    void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to
+    the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+    The functions get and put must each run in O(1) average time complexity.
+
+    Example 1:
+    Input:
+    ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+    [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+    Output:
+    [null, null, null, 1, null, -1, null, -1, 3, 4]
+
+    Explanation:
+    LRUCache lRUCache = new LRUCache(2);
+    lRUCache.put(1, 1); // cache is {1=1}
+    lRUCache.put(2, 2); // cache is {1=1, 2=2}
+    lRUCache.get(1);    // return 1
+    lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    lRUCache.get(2);    // returns -1 (not found)
+    lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    lRUCache.get(1);    // return -1 (not found)
+    lRUCache.get(3);    // return 3
+    lRUCache.get(4);    // return 4
+
+    Constraints:
+    1 <= capacity <= 3000
+    0 <= key <= 10^4
+    0 <= value <= 10^5
+    At most 2 * 10^5 calls will be made to get and put.
+
+    Plan:
+    Init: Create cache and capacity.
+    Get: Check if key is not in cache. If so, return -1. Otherwise, move key to the right end of the cache,
+    and return cache[key].
+    Put: Check if the key is in the cache. If so, delete it. Then add they key,value to the cache. Then move the key
+    to the right end of the cache (necessary?) Then check if len(cache) > capacity. If so, then discard the left-most
+    key in the cache. Return None.
+    Time: O(1) for get and put
+    Space: O(capacity)
+    Edge: None
+    '''
+
+    def __init__(self, capacity: int):
+        import collections
+        self.cache = collections.OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            del self.cache[key]
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(False)       # Deletes the left-most key
+        return None
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
