5ca0085906b47b24a3a7fdf3b913a34b60559e39
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-02-16 14:07:24 +0900

    023 Merge k Sorted Lists

---
+++ 023 - Merge k Sorted Lists/Attempt 4 - 2024.02.16.py
@@ -0,0 +1,92 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        '''
+        023 - Merge k Sorted Lists
+
+        https://leetcode.com/problems/merge-k-sorted-lists/
+
+        You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+        Merge all the linked-lists into one sorted linked-list and return it.
+
+        Example 1:
+        Input: lists = [[1,4,5],[1,3,4],[2,6]]
+        Output: [1,1,2,3,4,4,5,6]
+        Explanation: The linked-lists are:
+        [
+          1->4->5,
+          1->3->4,
+          2->6
+        ]
+        merging them into one sorted list:
+        1->1->2->3->4->4->5->6
+
+        Example 2:
+        Input: lists = []
+        Output: []
+
+        Example 3:
+        Input: lists = [[]]
+        Output: []
+
+        Constraints:
+        k == lists.length
+        0 <= k <= 10^4
+        0 <= lists[i].length <= 500
+        -10^4 <= lists[i][j] <= 10^4
+        lists[i] is sorted in ascending order.
+        The sum of lists[i].length will not exceed 10^4.
+
+        Plan:
+        Handle out-of-bounds
+        Create mergeSort function.
+        Loop while len(lists) > 1.
+            merged_lists = []
+            for i in range(0, len(lists), 2):
+                Grab l1 and l2 (handle out-of-bounds)
+                Call mergeSort function with (l1, l2), set to l3
+                Append l3 to merged_lists
+            lists = merged_lists
+        return lists[0]
+
+        Time: O(k log n)
+        Space: O(n)
+        Edge: Possible 0 len array
+        '''
+        if not lists:
+            return None
+
+        def mergeSort(l1, l2):
+            dummy = ListNode(-1)
+            l3 = dummy
+            while l1 and l2:
+                if l1.val <= l2.val:
+                    l3.next = l1
+                    l1 = l1.next
+                else:
+                    l3.next = l2
+                    l2 = l2.next
+                l3 = l3.next
+
+            if l1:
+                l3.next = l1
+            elif l2:
+                l3.next = l2
+            return dummy.next
+
+        while len(lists) > 1:
+            merged_lists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1) < len(lists) else None
+                l3 = mergeSort(l1, l2)
+                merged_lists.append(l3)
+            lists = merged_lists
+
+        return lists[0]
+
