b8f8e5a59b5db23f22ee5e2fc2c341a2a5e34630
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-02-18 20:38:37 +0900

    424 Longest Repeating Character Replacement

---
+++ 424 - Longest Repeating Character Replacement/Attempt 18 - 2024.02.18.py
@@ -0,0 +1,64 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        '''
+        424 - Longest Repeating Character Replacement
+
+        https://leetcode.com/problems/longest-repeating-character-replacement/
+
+        You are given a string s and an integer k.
+        You can choose any character of the string and change it to any other uppercase English character.
+        You can perform this operation at most k times.
+
+        Return the length of the longest substring containing the same letter you can get after performing the above
+        operations.
+
+        Input: s = "ABAB", k = 2
+        Output: 4
+        Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+        Input: s = "AABABBA", k = 1
+        Output: 4
+        Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+        The substring "BBBB" has the longest repeating letters, which is 4.
+
+        Constraints:
+
+        1 <= s.length <= 105
+        s consists of only uppercase English letters.
+        0 <= k <= s.length
+
+        Plan:
+        Sliding Window
+        Create s_window Counter dict.
+        l, r = 0, len(s)-1.
+        Loop while r < len(s) -1.
+            Add s[r] to s_window.
+            Check if (r - l + 1 - max(s_window.values)) < = k. If so, then the current string is valid,
+            so calc cur_len and update max_len. Otherwise, remove s[l] from s_window and l++ until the string is
+            valid again.
+            r++
+        Return longest_len.
+        Time: O(n)
+        Space: O(1)
+        Edge: k could be 0, return empty str.
+        '''
+        import collections
+        s_window = collections.Counter()
+        l, r = 0, 0
+        max_len = 0
+        while r < len(s):
+            s_window[s[r]] += 1
+            if (r - l + 1 - max(s_window.values())) <= k:
+                max_len = max(max_len, r-l+1)
+            else:
+                s_window[s[l]] -= 1
+                l += 1
+            r += 1
+        return max_len
+
+
+
+result = Solution()
+print(result.characterReplacement("ABAB", 2))       # 4
+print(result.characterReplacement("AABABBA", 1))    # 4
+
