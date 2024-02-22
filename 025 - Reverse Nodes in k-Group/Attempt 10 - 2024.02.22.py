6ba11ea981d61909d25dd8cb00ab342c5041df9e
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-02-22 15:08:09 +0900

    025 Reverse Nodes in k-Group

---
+++ 025 - Reverse Nodes in k-Group/Attempt 10 - 2024.02.22.py
@@ -0,0 +1,74 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        """
+        https://leetcode.com/problems/reverse-nodes-in-k-group/
+
+        Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+
+        k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is
+        not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+        You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+        Input: head = [1,2,3,4,5], k = 2
+        Output: [2,1,4,3,5]
+
+        Input: head = [1,2,3,4,5], k = 3
+        Output: [3,2,1,4,5]
+
+        Constraints:
+        The number of nodes in the list is n.
+        1 <= k <= n <= 5000
+        0 <= Node.val <= 1000
+
+        Plan:
+        Linked List
+        Create grab_kth_node(node, k) function. Returns node.
+        Setup dummy
+        Set left_seg to dummy.
+        Infinite loop.
+            Call grab_kth_node(left_seg, k). Set to mid_seg.
+            If mid_seg returns None, then we don't have any more nodes to reverse, so break.
+            Otherwise set right_seg = mid_seg.next
+            Set cur = left_seg.next.
+            Reverse cur by k. Stop the reversal when cur = right_seg.
+            Reattach mid_seg to the left_seg
+            Shift the left_seg pointer to right_seg.
+        Return dummy.next
+
+        Time: O(n)
+        Space: O(1)
+        Edge: None
+        """
+        def grab_kth_node(node,k):
+            while node and (k > 0):
+                node = node.next
+                k -= 1
+            return node
+
+        dummy = ListNode(-1, head)
+        left_seg = dummy
+        while True:
+            mid_seg = grab_kth_node(left_seg, k)
+            if not mid_seg:
+                break
+            right_seg = mid_seg.next
+
+            prev = right_seg
+            cur = left_seg.next
+            while (cur != right_seg):
+                temp = cur.next
+                cur.next = prev
+                prev = cur
+                cur = temp
+
+            temp2 = left_seg.next
+            left_seg.next = prev
+            left_seg = temp2
+
+        return dummy.next
