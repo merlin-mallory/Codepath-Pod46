e681fe27553ee28665667e2e42583125b119d031
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-03-06 12:53:02 +0900

    036 Valid Sudoku

---
+++ 036 - Valid Sudoku/Attempt 14 - 2024.03.06.py
@@ -0,0 +1,89 @@
+class Solution:
+    def isValidSudoku(self, board) -> bool:
+        """
+        Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the
+        following rules:
+
+        Each row must contain the digits 1-9 without repetition.
+        Each column must contain the digits 1-9 without repetition.
+        Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+        Note:
+
+        A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+        Only the filled cells need to be validated according to the mentioned rules.
+
+        Constraints:
+        board.length == 9
+        board[i].length == 9
+        board[i][j] is a digit 1-9 or '.'.
+        """
+        # Create row sets, col sets, and box sets
+        row_arr = [set() for _ in range(9)]
+        col_arr = [set() for _ in range(9)]
+        box_arr = [set() for _ in range(9)]
+        # Loop through board, and grab the current value.
+        for row in range(9):
+            for col in range(9):
+                cur = board[row][col]
+                # Handle the "." character by continuing
+                if cur == ".":
+                    continue
+                # Check if cur is not in the proper row. If so, add it to that set. Otherwise return False.
+                if cur not in row_arr[row]:
+                    row_arr[row].add(cur)
+                else:
+                    return False
+                # Check if cur is not in the proper col. If so, add it to that set. Otherwise return False.
+                if cur not in col_arr[col]:
+                    col_arr[col].add(cur)
+                else:
+                    return False
+                # Check if cur is not in the proper box. If so, add it to that set. Otherwise return False.
+                # Box calculation formula: box = ((row * 9) // 3) + (col // 3)
+                box = ((row // 3) * 3) + (col // 3)
+                if cur not in box_arr[box]:
+                    box_arr[box].add(cur)
+                else:
+                    return False
+        # If we complete the loop, then we've validated the entire board, so return True.
+        return True
+
+result = Solution()
+
+board1 = [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+    , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+    , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+    , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+    , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+    , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+    , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+    , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+    , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+
+print(result.isValidSudoku(board1))  # True
+
+board2 = [["8", "3", ".", ".", "7", ".", ".", ".", "."],
+          ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+          [".", "9", "8", ".", ".", ".", ".", "6", "."],
+          ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+          ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+          ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+          [".", "6", ".", ".", ".", ".", "2", "8", "."],
+          [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+          [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+
+print(result.isValidSudoku(board2))  # False
+# Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8.
+# Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+board3 = [[".", ".", ".", ".", "5", ".", ".", "1", "."],
+          [".", "4", ".", "3", ".", ".", ".", ".", "."],
+          [".", ".", ".", ".", ".", "3", ".", ".", "1"],
+          ["8", ".", ".", ".", ".", ".", ".", "2", "."],
+          [".", ".", "2", ".", "7", ".", ".", ".", "."],
+          [".", "1", "5", ".", ".", ".", ".", ".", "."],
+          [".", ".", ".", ".", ".", "2", ".", ".", "."],
+          [".", "2", ".", "9", ".", ".", ".", ".", "."],
+          [".", ".", "4", ".", ".", ".", ".", ".", "."]]
+
+print(result.isValidSudoku(board3))  # False
