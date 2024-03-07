235c144cc876a8abd963ea48a02d488d16b1e4a2
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-03-07 18:03:21 +0900

    105 Construct  Binary Tree from Preorder and Inorder Traversal

---
+++ 105 - Construct Binary Tree from Preorder and Inorder Traversal/Attempt 5 - 2024.03.07.py
@@ -0,0 +1,60 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        '''
+        105 - Construct Binary Tree from Preorder and Inorder Traversal
+
+        https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+        Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and
+        inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+        Example 1:
+        Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+        Output: [3,9,20,null,null,15,7]
+
+        Example 2:
+        Input: preorder = [-1], inorder = [-1]
+        Output: [-1]
+
+        Constraints:
+        1 <= preorder.length <= 3000
+        inorder.length == preorder.length
+        -3000 <= preorder[i], inorder[i] <= 3000
+        preorder and inorder consist of unique values.
+        Each value of inorder also appears in preorder.
+        preorder is guaranteed to be the preorder traversal of the tree.
+        inorder is guaranteed to be the inorder traversal of the tree.
+
+        Plan:
+        Tree Traversal with DFS
+        Create inorder_dict as Counter, init with inorder.
+        Create dfs(l, r)
+            if (l > r): return None
+            node = preorder.pop(0)
+            m = inorder_dict[node.val]
+            node.left = dfs(l, m-1)
+            node.right = dfs(m+1, r)
+            return node
+        return dfs(0, len(preorder)-1)
+        Time: Amortized O(n)
+        Space: O(h) or O(1), depending upon if the output space counts
+        Edge: None
+        '''
+        import collections
+        inorder_dict = {}
+        for i, val in enumerate(inorder):
+            inorder_dict[val] = i
+        def dfs(l, r):
+            if (l > r): return None
+            node = TreeNode(preorder.pop(0))
+            m = inorder_dict[node.val]
+            node.left = dfs(l, m-1)
+            node.right = dfs(m+1, r)
+            return node
+        return dfs(0, len(inorder)-1)
