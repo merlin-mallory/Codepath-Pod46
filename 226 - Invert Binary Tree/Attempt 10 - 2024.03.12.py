8d9617f131c63c6acd5d0fb886644fac909e37de
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-03-12 10:30:25 +0900

    226 Invert Binary Tree

---
+++ 226 - Invert Binary Tree/Attempt 10 - 2024.03.12.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        '''
+        226 - Invert Binary Tree
+
+        https://leetcode.com/problems/invert-binary-tree/
+
+        Given the root of a binary tree, invert the tree, and return its root.
+
+        Input: root = [4,2,7,1,3,6,9]
+        Output: [4,7,2,9,6,3,1]
+
+        Input: root = [2,1,3]
+        Output: [2,3,1]
+
+        Input: root = []
+        Output: []
+
+        Constraints:
+
+        The number of nodes in the tree is in the range [0, 100].
+        -100 <= Node.val <= 100
+
+        Plan:
+        Tree Traversal DFS
+        Time: O(n)
+        Space: O(h)
+        Edge: Could be 0 nodes in root
+        '''
+        if not root: return None
+        root.left = self.invertTree(root.left)
+        root.right = self.invertTree(root.right)
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        return root
+
