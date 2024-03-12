7f7eca7a711e7210f8ba1b93ab996aaececc1fc9
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-03-12 11:48:43 +0900

    1448 Count Good Nodes in Binary Tree

---
+++ 1448 - Count Good Nodes in Binary Tree/Attempt 6 - 2024.03.12.py
@@ -0,0 +1,66 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        '''
+        1448 - Count Good Nodes in Binary Tree
+
+        https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+
+        Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no
+        nodes with a value greater than X.
+
+        Return the number of good nodes in the binary tree.
+
+        Example 1:
+        Input: root = [3,1,4,3,null,1,5]
+        Output: 4
+        Explanation: Nodes in blue are good.
+        Root Node (3) is always a good node.
+        Node 4 -> (3,4) is the maximum value in the path starting from the root.
+        Node 5 -> (3,4,5) is the maximum value in the path
+        Node 3 -> (3,1,3) is the maximum value in the path.
+
+        Example 2:
+        Input: root = [3,3,null,4,2]
+        Output: 3
+        Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+
+        Example 3:
+        Input: root = [1]
+        Output: 1
+        Explanation: Root is considered as good.
+
+        Constraints:
+        The number of nodes in the binary tree is in the range [1, 10^5].
+        Each node's value is between [-10^4, 10^4].
+
+        Plan:
+        Tree Traversal with DFS
+
+        Create dfs(node, cur_max). Returns an int indicating the number of good nodes underneath it.
+            Base Case 1: If not node, then return 0
+            cur_good = 1 if (node.val <= cur_max) else 0
+            new_max = max(cur_max, node.val)
+            Recursive Case 1: left_good = dfs(node.left, new_max)
+            Recursive Case 2: right_good = dfs(node.right, new_max)
+            return cur_good + left_good + right_good
+        return dfs(node, node.val)
+
+        Time: O(n)
+        Space: O(h)
+        Edge: None
+        '''
+        def dfs(node, cur_max):
+            if not node: return 0
+            cur_good = 1 if (node.val >= cur_max) else 0
+            new_max = max(cur_max, node.val)
+            left_good = dfs(node.left, new_max)
+            right_good = dfs(node.right, new_max)
+            return cur_good + left_good + right_good
+        return dfs(root, root.val)
+
