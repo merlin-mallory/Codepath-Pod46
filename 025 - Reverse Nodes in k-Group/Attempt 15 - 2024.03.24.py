865d7f42fcd7cfbac533586053d93bfcd3571d4d
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-03-24 16:10:07 +0900

    025 Reverse Nodes in k-Group

---
+++ 025 - Reverse Nodes in k-Group/Attempt 15 - 2024.03.24.py
@@ -0,0 +1,66 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        """
+        https://leetcode.com/problems/reverse-nodes-in-k-group/
+
+        Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+
+        k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is
+        not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+        You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+        Input: head = [1,2,3,4,5], k = 2
+        Output: [2,1,4,3,5]
+
+        Input: head = [1,2,3,4,5], k = 3
+        Output: [3,2,1,4,5]
+
+        Constraints:
+        The number of nodes in the list is n.
+        1 <= k <= n <= 5000
+        0 <= Node.val <= 1000
+
+        Plan:
+        Linked List Traversal
+        Create get_kth_node function.
+        Set left_seg = head.
+        Loop while True.
+            Call get_kth_node(left_seg). Set equal to mid_seg.
+            If not mid_seg, break.
+            Set right_seg = mid_seg.next
+            Reverse mid_seg
+            Reattach left_seg, mid_seg, right_seg.
+            Set left_seg = right_seg
+        Time: O(n)
+        Space: O(1)
+        Edge: None
+        """
+        def get_kth_node(node, k):
+            while node and (k > 0):
+                node = node.next
+                k -= 1
+            return node
+        dummy = ListNode(-1, head)
+        left_seg = dummy
+        while True:
+            mid_seg = get_kth_node(left_seg, k)
+            if not mid_seg: break
+            right_seg = mid_seg.next
+            prev = right_seg
+            cur = left_seg.next
+            while cur != right_seg:
+                temp = cur.next
+                cur.next = prev
+                prev = cur
+                cur = temp
+            temp = left_seg.next
+            left_seg.next = prev
+            right_seg = temp
+            left_seg = right_seg
+        return dummy.next
