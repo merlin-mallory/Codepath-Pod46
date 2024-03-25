7a3dff4b458310f3abd8ee48756b48819c9bf907
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-03-25 17:04:26 +0900

    017 Letter Combinations of a Phone Number

--- 017 - Letter Combinations of a Phone Number/Attempt 6 - 2024.03.25.py
+++ 017 - Letter Combinations of a Phone Number/Attempt 6 - 2024.03.25.py
@@ -24,9 +24,40 @@ class Solution:
         Constraints:
         0 <= digits.length <= 4
         digits[i] is a digit in the range ['2', '9'].
+
+        Time: O(4^n)
+        Space: O(h), worst case O(n)
+        Edge: If digits is empty, return an empty list
         """
+        if not digits:
+            return []
+
+        digits_to_chars = {
+            '2': ["a", "b", "c"],
+            '3': ["d", "e", "f"],
+            '4': ["g", "h", "i"],
+            '5': ["j", "k", "l"],
+            '6': ["m", "n", "o"],
+            '7': ["p", "q", "r", "s"],
+            '8': ["t", "u", "v"],
+            '9': ["w", "x", "y", "z"]
+        }
+        stack = []
+        final_arr = []
+        def backtrack(i):
+            if len(stack) == len(digits):
+                final_arr.append("".join(stack))
+                return
+            cur_digit = digits[i]
+            for d in digits_to_chars[cur_digit]:
+                stack.append(d)
+                backtrack(i + 1)
+                stack.pop()
+        backtrack(0)
+        return final_arr
+
 
 result = Solution()
 print(result.letterCombinations("23"))  # ["ad","ae","af","bd","be","bf","cd","ce","cf"]
-print(result.letterCombinations(""))    # []
-print(result.letterCombinations("2"))   # ["a","b","c"]
+print(result.letterCombinations(""))  # []
+print(result.letterCombinations("2"))  # ["a","b","c"]
