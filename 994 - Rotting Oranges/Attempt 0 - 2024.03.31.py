70b5f48d260f68099b6d67b562f3cd4b9d225eba
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-03-31 17:54:23 +0900

    994 Rotting Oranges

--- 994 - Rotting Oranges/Attempt 0 - 2024.03.31.py
+++ 994 - Rotting Oranges/Attempt 0 - 2024.03.31.py
@@ -1,4 +1,6 @@
 from typing import List
+
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         """
@@ -29,20 +31,51 @@ class Solution:
         n == grid[i].length
         1 <= m, n <= 10
         grid[i][j] is 0, 1, or 2
+        Time: O(m*n)
+        Space: O(m*n)
+        Edge: None
         """
+        import collections
+        deque = collections.deque()
+        time = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        fresh_orange_count = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    fresh_orange_count += 1
+                elif grid[r][c] == 2:
+                    deque.append((r, c))
+
+        while deque and (fresh_orange_count > 0):
+            time += 1
+            for i in range(len(deque)):
+                r, c = deque.popleft()
+                grid[r][c] = 2
+                adj = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for rmod, cmod in adj:
+                    row, col = r + rmod, c + cmod
+                    if (rows > row >= 0) and (cols > col >= 0) and (grid[row][col] == 1):
+                        grid[row][col] = 2
+                        fresh_orange_count -= 1
+                        deque.append((row, col))
+
+        return time if (fresh_orange_count == 0) else -1
 
 
 solution = Solution()
 
-grid1 = [[2,1,1],
-         [1,1,0],
-         [0,1,1]]
+grid1 = [[2, 1, 1],
+         [1, 1, 0],
+         [0, 1, 1]]
 print(solution.orangesRotting(grid1))  # 4
 
-grid2 = [[2,1,1],
-         [0,1,1],
-         [1,0,1]]
+grid2 = [[2, 1, 1],
+         [0, 1, 1],
+         [1, 0, 1]]
 print(solution.orangesRotting(grid2))  # -1
 
-grid3 = [[0,2]]
+grid3 = [[0, 2]]
 print(solution.orangesRotting(grid3))  # 0
