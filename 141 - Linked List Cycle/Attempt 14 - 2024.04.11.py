29a4cf961b46580e7b4fe0a8373124fdbd03c895
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-04-11 18:55:06 +0900

    141 Linked List Cycle

---
+++ 141 - Linked List Cycle/Attempt 14 - 2024.04.11.py
@@ -0,0 +1,73 @@
+from typing import Optional, List
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def create_list(elements: List[int]) -> Optional[ListNode]:
+    """
+    Convert an array of elements into a linked list and return the head of the list.
+    """
+    head = None
+    current = None
+    for element in elements:
+        if not head:
+            head = ListNode(element)
+            current = head
+        else:
+            current.next = ListNode(element)
+            current = current.next
+    return head
+
+def print_list(head: Optional[ListNode]) -> None:
+    """
+    Print the elements of a linked list.
+    """
+    current = head
+    while current:
+        print(current.val, end=" ")
+        current = current.next
+    print()
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        '''
+        https://leetcode.com/problems/linked-list-cycle/
+
+        Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+        There is a cycle in a linked list if there is some node in the list that can be reached again by continuously
+        following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer
+        is connected to. Note that pos is not passed as a parameter.
+
+        Return true if there is a cycle in the linked list. Otherwise, return false.
+
+        Input: head = [3,2,0,-4], pos = 1
+        Output: true
+        Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+        Input: head = [1,2], pos = 0
+        Output: true
+        Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+        Input: head = [1], pos = -1
+        Output: false
+        Explanation: There is no cycle in the linked list.
+
+        The number of the nodes in the list is in the range [0, 10^4].
+        -10^5 <= Node.val <= 10^5
+        pos is -1 or a valid index in the linked-list.
+
+        Plan:
+        Linked List Traversal
+        Time: O(n)
+        Space: O(1)
+        Edge: Could be 0 nodes in head
+        '''
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast: return True
+        return False
