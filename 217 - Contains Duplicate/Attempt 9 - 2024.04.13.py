c2b5b0bbb57ff9c84cd9074f4d5f0a514de8ead9
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-04-13 10:49:04 +0900

    217 Contains Duplicate

---
+++ 217 - Contains Duplicate/Attempt 9 - 2024.04.13.py
@@ -0,0 +1,30 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        '''
+        Given an integer array nums, return true if any value appears at least twice in the array, and return false
+        if every element is distinct.
+
+        Input: nums = [1,2,3,1]
+        Output: true
+
+        Input: nums = [1,2,3,4]
+        Output: false
+
+        Input: nums = [1,1,1,3,3,4,3,2,4,2]
+        Output: true
+
+        Constraints:
+        1 <= nums.length <= 10^5
+        -10^9 <= nums[i] <= 10^9a
+
+        Plan:
+        Hashmap
+        Time: O(n)
+        Space: O(n)
+        Edge: None
+        '''
+        nums_set = set()
+        for num in nums:
+            if num in nums_set: return True
+            nums_set.add(num)
+        return False
