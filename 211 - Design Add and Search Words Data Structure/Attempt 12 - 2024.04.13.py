b3672c634258e860c8a8182c4a9059583fab9a04
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-04-13 12:17:20 +0900

    211 Design Add and Search Words Data Structure

---
+++ 211 - Design Add and Search Words Data Structure/Attempt 12 - 2024.04.13.py
@@ -0,0 +1,78 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class WordDictionary:
+    '''
+    211 - Design Add and Search Words Data Structure
+
+    https://leetcode.com/problems/design-add-and-search-words-data-structure/
+
+    Design a data structure that supports adding new words and finding if a string matches any previously added string.
+
+    Implement the WordDictionary class:
+
+    WordDictionary() Initializes the object.
+    void addWord(word) Adds word to the data structure, it can be matched later.
+    bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise.
+    word may contain dots '.' where dots can be matched with any letter.
+
+    Example:
+    Input
+    ["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+    [[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+    Output
+    [null,null,null,null,false,true,true,true]
+
+    Explanation
+    WordDictionary wordDictionary = new WordDictionary();
+    wordDictionary.addWord("bad");
+    wordDictionary.addWord("dad");
+    wordDictionary.addWord("mad");
+    wordDictionary.search("pad"); // return False
+    wordDictionary.search("bad"); // return True
+    wordDictionary.search(".ad"); // return True
+    wordDictionary.search("b.."); // return True
+
+    Constraints:
+    1 <= word.length <= 25
+    word in addWord consists of lowercase English letters.
+    word in search consist of '.' or lowercase English letters.
+    There will be at most 2 dots in word for search queries.
+    At most 10^4 calls will be made to addWord and search.
+
+    Plan:
+    Backtracking with Trie
+    Time: O(25) for add, O(n * 25 * 2^n) for search
+    Space: O(m*n) for add (where m = # of words, and n = avg length of word), O(n) for search
+    Edge: None
+    '''
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children: cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.is_end = True
+        return None
+
+    def search(self, word: str) -> bool:
+        def dfs(node, i):
+            for j in range(i, len(word)):
+                c = word[j]
+                if c == ".":
+                    for child in node.children.values():
+                        if dfs(child, j+1): return True
+                    return False
+                elif c not in node.children: return False
+                node = node.children[c]
+            return node.is_end
+        return dfs(self.root, 0)
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
