a2ab6a4a1844a29d7722dcafa54362a5facc5f22
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-04-22 12:16:25 +0900

    1584 Min Cost to Connect All Points

--- 1584 - Min Cost to Connect All Points/Attempt 1 - 2024.04.22.py
+++ 1584 - Min Cost to Connect All Points/Attempt 1 - 2024.04.22.py
@@ -21,10 +21,10 @@ class Solution:
         All pairs (xi, yi) are distinct.
 
         Plan:
-        Graph Traversal with Minheap and Prim's Algorithm
-        Time: O(n^2 * log n)
+        Graph Traversal with Minheap
+        Time: O(n log n)
         Space: O(n)
-        edge: None
+        Edge: None
         '''
         import collections, heapq
 
@@ -49,34 +49,6 @@ class Solution:
                 if neighbor not in visited_set:
                     heapq.heappush(minheap, neighbor)
         return total_cost
-
-# Neetcode solution:
-# class Solution:
-#     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-#         N = len(points)
-#         adj = {i: [] for i in range(N)}  # i : list of [cost, node]
-#         for i in range(N):
-#             x1, y1 = points[i]
-#             for j in range(i + 1, N):
-#                 x2, y2 = points[j]
-#                 dist = abs(x1 - x2) + abs(y1 - y2)
-#                 adj[i].append([dist, j])
-#                 adj[j].append([dist, i])
-#
-#         # Prim's
-#         res = 0
-#         visit = set()
-#         minH = [[0, 0]]  # [cost, point]
-#         while len(visit) < N:
-#             cost, i = heapq.heappop(minH)
-#             if i in visit:
-#                 continue
-#             res += cost
-#             visit.add(i)
-#             for neiCost, nei in adj[i]:
-#                 if nei not in visit:
-#                     heapq.heappush(minH, [neiCost, nei])
-#         return res
 
 solution = Solution()
 print(solution.minCostConnectPoints([[0,0],[2,2],[3,10],[5,2],[7,0]]))
