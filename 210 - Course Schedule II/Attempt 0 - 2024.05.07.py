74a5ccc0b12a50a598c8f34db256002430dde42b
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-05-08 12:09:27 +0900

    210 Course Schedule II

--- 210 - Course Schedule II/Attempt 0 - 2024.05.07.py
+++ 210 - Course Schedule II/Attempt 0 - 2024.05.07.py
@@ -65,7 +65,7 @@ print(solution.findOrder(2, [[1, 0]]))
 # So the correct course order is [0,1].
 
 print(solution.findOrder(4, [[1, 0], [2, 0], [3, 1], [3, 2]]))
-# [0,2,1,3]
+# [0,1,2,3]
 # Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and
 # 2. Both courses 1 and 2 should be taken after you finished course 0. So one correct course order is [0,1,2,3].
 # Another correct ordering is [0,2,1,3].
