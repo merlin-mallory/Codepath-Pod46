35af54d687b5226945ab22500838647d10c8c212
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-05-15 19:27:56 +0900

    070 Climbing Stairs

---
+++ 070 Climbing Stairs/Attempt 0 - 2025.05.15.py
@@ -0,0 +1,39 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        '''
+        070 - Climbing Stairs
+
+        https://leetcode.com/problems/climbing-stairs/
+
+        You are climbing a staircase. It takes n steps to reach the top.
+
+        Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+        Constraints:
+        1 <= n <= 45
+
+        Plan:
+        Dynamic Programming
+        Time: O(n)
+        Space: O(1)
+        Edge: None
+        '''
+        one, two = 1, 1
+        for i in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
+
+solution = Solution()
+
+print(solution.climbStairs(2))  # 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+
+print(solution.climbStairs(3))  # 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
