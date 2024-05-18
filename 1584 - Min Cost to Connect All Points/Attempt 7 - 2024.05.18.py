bfbb98df096d8a63218ed8ee565bd6b6e60831bd
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-05-18 20:19:24 +0900

    1584 Min Cost to Connect All Points

--- 1584 - Min Cost to Connect All Points/Attempt 7 - 2024.05.18.py
+++ 1584 - Min Cost to Connect All Points/Attempt 7 - 2024.05.18.py
@@ -21,62 +21,33 @@ class Solution:
         All pairs (xi, yi) are distinct.
 
         Plan:
-        Graph Traversal with Minheap and Prim's Algorithm
-        Time: O(n^2 * log n)
+        Graph Traversal with BFS (Prim's Algo)
+        Time: O(n^2 log n)
         Space: O(n^2)
-        edge: None
+        Edge: None
         '''
         import collections, heapq
-
         source_to_dest = collections.defaultdict(list)
         for i in range(len(points)):
             x1, y1 = points[i]
             for j in range(i+1, len(points)):
                 x2, y2 = points[j]
-                dist = abs(x1 - x2) + abs(y1 - y2)
+                dist = abs(x1-x2) + abs(y1-y2)
                 source_to_dest[i].append((dist, j))
                 source_to_dest[j].append((dist, i))
-
+        minheap = [(0, 0)]  # [dist, point]
         visited_set = set()
-        minheap = [(0, 0)]    # (dist, point)
         total_cost = 0
-        while len(visited_set) != len(points):
-            cur_dist, cur_point = heapq.heappop(minheap)
-            if cur_point in visited_set: continue
-            visited_set.add(cur_point)
-            total_cost += cur_dist
-            for neighbor in source_to_dest[cur_point]:
-                if neighbor not in visited_set:
-                    heapq.heappush(minheap, neighbor)
+        while minheap and (len(visited_set) != len(points)):
+            c1, p1 = heapq.heappop(minheap)
+            if p1 in visited_set: continue
+            visited_set.add(p1)
+            total_cost += c1
+            for c2, p2 in source_to_dest[p1]:
+                if p2 not in visited_set:
+                    heapq.heappush(minheap, (c2, p2))
         return total_cost
 
-# Neetcode solution:
-# class Solution:
-#     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-#         N = len(points)
-#         adj = {i: [] for i in range(N)}  # i : list of [cost, node]
-#         for i in range(N):
-#             x1, y1 = points[i]
-#             for j in range(i + 1, N):
-#                 x2, y2 = points[j]
-#                 dist = abs(x1 - x2) + abs(y1 - y2)
-#                 adj[i].append([dist, j])
-#                 adj[j].append([dist, i])
-#
-#         # Prim's
-#         res = 0
-#         visit = set()
-#         minH = [[0, 0]]  # [cost, point]
-#         while len(visit) < N:
-#             cost, i = heapq.heappop(minH)
-#             if i in visit:
-#                 continue
-#             res += cost
-#             visit.add(i)
-#             for neiCost, nei in adj[i]:
-#                 if nei not in visit:
-#                     heapq.heappush(minH, [neiCost, nei])
-#         return res
 
 solution = Solution()
 print(solution.minCostConnectPoints([[0,0],[2,2],[3,10],[5,2],[7,0]]))
