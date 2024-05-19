69fd285e4f2d71a20aa73057b9d9f393228da5e4
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-05-19 14:40:23 +0900

    039 Combination Sum

---
+++ 039 - Combination Sum/Attempt 9 - 2024.05.19.py
@@ -0,0 +1,65 @@
+from typing import List
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        '''
+        039 - Combination Sum
+
+        https://leetcode.com/problems/combination-sum/
+
+        Given an array of distinct integers candidates and a target integer target, return a list of all unique
+        combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+        The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the
+        frequency of at least one of the chosen numbers is different.
+
+        The test cases are generated such that the number of unique combinations that sum up to target is less than
+        150 combinations for the given input.
+
+        Example 1:
+        Input: candidates = [2,3,6,7], target = 7
+        Output: [[2,2,3],[7]]
+        Explanation:
+        2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+        7 is a candidate, and 7 = 7.
+        These are the only two combinations.
+
+        Example 2:
+        Input: candidates = [2,3,5], target = 8
+        Output: [[2,2,2,2],[2,3,3],[3,5]]
+
+        Example 3:
+        Input: candidates = [2], target = 1
+        Output: []
+
+        Constraints:
+        1 <= candidates.length <= 30
+        2 <= candidates[i] <= 40
+        All elements of candidates are distinct.
+        1 <= target <= 40
+
+        Plan:
+        Backtracking
+        Time: O(2^n)
+        Space: O(n)
+        Edge: None
+        '''
+        stack = []
+        final_arr = []
+        def backtrack(i, cur_sum):
+            if cur_sum == target:
+                final_arr.append(stack[:])
+                return
+            if (cur_sum > target) or (i == len(candidates)):
+                return
+            stack.append(candidates[i])
+            backtrack(i, cur_sum + candidates[i])
+            stack.pop()
+            backtrack(i+1, cur_sum)
+        backtrack(0, 0)
+
+        return final_arr
+
+result = Solution()
+print(result.combinationSum([2,3,6,7], 7))  # [[2,2,3],[7]]
+print(result.combinationSum([2,3,5],8))     # [[2,2,2,2],[2,3,3],[3,5]]
+print(result.combinationSum([2],1))         # []
