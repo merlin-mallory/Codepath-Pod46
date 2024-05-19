5e287367cf9892e9b876bfe9e3aa13dd7c57cc5c
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-05-19 13:30:04 +0900

    070 Climbing Stairs

--- 070 Climbing Stairs/Attempt 0 - 2025.05.15.py
+++ 070 Climbing Stairs/Attempt 0 - 2025.05.15.py
@@ -37,3 +37,15 @@ print(solution.climbStairs(3))  # 3
 # 1. 1 step + 1 step + 1 step
 # 2. 1 step + 2 steps
 # 3. 2 steps + 1 step
+
+# Classic DP solution
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         if n == 1: return 1
+#         dp = [0] * (n+1)
+#         dp[1] = 1
+#         dp[2] = 2
+#         for i in range(3, n+1):
+#             dp[i] = dp[i-1] + dp[i-2]
+#         return dp[n]
+#
