86530683d6981af436a983169eb891351e9d17b5
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-05-24 17:23:15 +0900

    198 House Robber

--- 198 - House Robber/Attempt 0 - 2025.05.23.py
+++ 198 - House Robber/Attempt 0 - 2025.05.23.py
@@ -18,11 +18,10 @@ class Solution:
         1 <= nums.length <= 100
         0 <= nums[i] <= 400
         '''
-        # Not finished...
         if len(nums) == 1: return nums[0]
         dp = [0] * len(nums)
         dp[0] = nums[0]
-        dp[1] = nums[1]
+        dp[1] = max(nums[0], nums[1])
         for i in range(2, len(nums)):
             dp[i] = max(nums[i] + dp[i-2], dp[i-1])
         return dp[-1]
