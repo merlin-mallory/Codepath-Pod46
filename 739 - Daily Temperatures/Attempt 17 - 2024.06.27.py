ae4b06814d140f27d37e5df9308ebe2116a7c734
Merlin Mallory <merlin.mallory.coder@gmail.com>
2024-06-27 10:44:43 +0900

    100 Same Tree

---
+++ 739 - Daily Temperatures/Attempt 17 - 2024.06.27.py
@@ -0,0 +1,44 @@
+from typing import List
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        '''
+        Given an array of integers temperatures represents the daily temperatures, return an array answer such that
+        answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is
+        no future day for which this is possible, keep answer[i] == 0 instead.
+
+        Example 1:
+        Input: temperatures = [73,74,75,71,69,72,76,73]
+        Output: [1,1,4,2,1,1,0,0]
+
+        Example 2:
+        Input: temperatures = [30,40,50,60]
+        Output: [1,1,1,0]
+
+        Example 3:
+        Input: temperatures = [30,60,90]
+        Output: [1,1,0]
+
+        Constraints:
+        1 <= temperatures.length <= 10^5
+        30 <= temperatures[i] <= 100
+
+        Plan:
+        Stack
+        Time: O(n)
+        Space: O(n)
+        Edge: None, but make sure if no future day is possible, answer[i] should be 0
+        '''
+        stack = []
+        answer = [0] * len(temperatures)
+        for i, val in enumerate(temperatures):
+            while stack and (val > stack[-1][1]):
+                stack_i, stack_v = stack.pop()
+                answer[stack_i] = i - stack_i
+            stack.append((i, val))
+        return answer
+
+
+result = Solution()
+print(result.dailyTemperatures([73,74,75,71,69,72,76,73]))  # [1,1,4,2,1,1,0,0]
+print(result.dailyTemperatures([30,40,50,60]))              # [1,1,1,0]
+print(result.dailyTemperatures([30,60,90]))                 # [1,1,0]
